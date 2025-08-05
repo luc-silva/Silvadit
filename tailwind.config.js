2e8399b3c37529b71f058a7f39794de22cea5e02
Lucas Santos <lucas.silva1203@outlook.com>
2025-08-05 13:56:41 -0300

    fix: change colors schemes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', // importante para temas
-  content: ['./src/**/*.{js,ts,jsx,tsx}'], // ajuste o path conforme sua estrutura
+  darkMode: 'class',
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        base: {
-          bg: 'var(--color-bg)',
-          surface: 'var(--color-surface)',
-          text: 'var(--color-text)',
-          muted: 'var(--color-muted)',
-          border: 'var(--color-border)',
-          primary: 'var(--color-primary)',
-          accent: 'var(--color-accent)',
-        },
+        bg: 'var(--color-bg)',
+        surface: 'var(--color-surface)',
+        text: 'var(--color-text)',
+        title: 'var(--color-title)',
+        subtitle: 'var(--color-subtitle)',
+        border: 'var(--color-border)',
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
       },
     },
   },
