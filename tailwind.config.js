ca610a63851f975c76b3c7597c246dac10dbf405
Lucas Santos <lucas.silva1203@outlook.com>
2025-08-08 13:59:07 -0300

    feat: add new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,31 @@ module.exports = {
         border: 'var(--color-border)',
         primary: 'var(--color-primary)',
         secondary: 'var(--color-secondary)',
+
+        success: {
+          DEFAULT: 'var(--color-success-bg)',
+          title: 'var(--color-success-title)',
+          text: 'var(--color-success-text)',
+          subtitle: 'var(--color-success-subtitle)',
+        },
+        warning: {
+          DEFAULT: 'var(--color-warning-bg)',
+          title: 'var(--color-warning-title)',
+          text: 'var(--color-warning-text)',
+          subtitle: 'var(--color-warning-subtitle)',
+        },
+        danger: {
+          DEFAULT: 'var(--color-danger-bg)',
+          title: 'var(--color-danger-title)',
+          text: 'var(--color-danger-text)',
+          subtitle: 'var(--color-danger-subtitle)',
+        },
+        info: {
+          DEFAULT: 'var(--color-info-bg)',
+          title: 'var(--color-info-title)',
+          text: 'var(--color-info-text)',
+          subtitle: 'var(--color-info-subtitle)',
+        },
       },
     },
   },
