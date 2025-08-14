a5da284c5a911a03945d2af2d4c37360cff2bb06
Lucas Santos <lucas.silva1203@outlook.com>
2025-08-14 12:21:57 -0300

    feat: save and retrieve session from localstorage

--- src/hooks/useApp/index.tsx
+++ src/hooks/useApp/index.tsx
@@ -1,12 +1,43 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useToast } from '../useToast';
 
 export const useApp = () => {
   const [session, setSession] = useState<ISession | null>(null);
 
+  const handleLogout = () => {
+    setSession(null);
+    window.localStorage.removeItem('session');
+  };
+
+  const saveSessionOnLocal = (data: ISession) => {
+    const parsed = JSON.stringify(data);
+    window.localStorage.setItem('session', parsed);
+  };
+
+  const retriveSessionFromLocal = (): ISession | null => {
+    const retrievedSession = window.localStorage.getItem('session');
+    if (retrievedSession) {
+      const parsed: ISession = JSON.parse(retrievedSession);
+
+      return parsed;
+    }
+
+    return null;
+  };
+
+  useEffect(() => {
+    const session = retriveSessionFromLocal();
+    if (session) {
+      setSession(session);
+    }
+  }, []);
+
   return {
     ...useToast(),
     session,
     setSession,
+    handleLogout,
+    saveSessionOnLocal,
+    retriveSessionFromLocal,
   };
 };
